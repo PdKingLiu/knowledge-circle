04b54610bef202cb15878b759670199ead21f14e
PdKingLiu <931942280@qq.com>
2019-09-05 23:26:01 +0800

    配置Bmob云后端

--- app/src/main/java/com/competition/pdking/knowledgecircle/MainApp.java
+++ app/src/main/java/com/competition/pdking/knowledgecircle/MainApp.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.competition.pdking.loginandregister.BuildConfig;
 
+import cn.bmob.v3.Bmob;
+
 /**
  * @author liupeidong
  * Created on 2019/9/5 22:58
@@ -19,5 +21,6 @@ public class MainApp extends Application {
             ARouter.openDebug();
         }
         ARouter.init(this);
+        Bmob.initialize(this, "060f143283bcbc6daa1fc0b82c9e0589");
     }
 }
--- loginandregister/src/main/java/com/competition/pdking/loginandregister/LayoutActivityMainActivity.java
+++ loginandregister/src/main/java/com/competition/pdking/loginandregister/LayoutActivityMainActivity.java
@@ -2,11 +2,15 @@ package com.competition.pdking.loginandregister;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
+
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.SaveListener;
 
 @Route(path = "/login/activity")
 public class LayoutActivityMainActivity extends AppCompatActivity {
@@ -21,4 +25,19 @@ public class LayoutActivityMainActivity extends AppCompatActivity {
         ARouter.getInstance().inject(this);
         Toast.makeText(this, "key3" + key3, Toast.LENGTH_SHORT).show();
     }
+
+    public void click(View view) {
+        Bean bean = new Bean(20, "刘沛栋", "安徽" );
+        bean.save(new SaveListener<String>() {
+            @Override
+            public void done(String s, BmobException e) {
+                if (e == null) {
+                    Toast.makeText(LayoutActivityMainActivity.this, s, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LayoutActivityMainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
 }
